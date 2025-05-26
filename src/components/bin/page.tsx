c95ba9080da7a5b5c74d1ee288e3ca0b8a522d60
Hahhen <arthurmp2007@hotmail.com>
2025-05-26 15:09:56 -0300

    Footer update

--- src/components/bin/page.tsx
+++ src/components/bin/page.tsx
@@ -46,7 +46,7 @@ export default function Bin() {
 
   return (
 
-    <main className="font-mono w-full m-auto flex flex-col mt-20 bg-card/80 py-6 rounded-2xl px-2 border md:flex-row md:gap-[2rem] md:justify-around md:p-5">
+    <main className="font-mono w-full m-auto flex flex-col mt-6 bg-card/80 py-6 rounded-2xl px-2 border md:flex-row md:gap-[2rem] md:justify-around md:p-5">
       <section className=" md:flex md:flex-col md:justify-center md:m-auto">
 
         <h2 className="m-auto font-serif text-4xl text-center mb-5">
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Footer() {
@@ -8,11 +9,8 @@ export default function Footer() {
         <div className="container">
 
           <div className="grid grid-cols-2 gap-8 lg:grid-cols-6">
-            <div className="col-span-2 mb-8 lg:mb-0">
+            <div className="col-span-2 lg:mb-0">
               <div className="flex items-center gap-2 flex-col lg:flex-row lg:justify-start">
-                <a href="https://shadcnblocks.com">
-
-                </a>
                 <h1 className="text-3xl font-serif mb-5">Computadores Revolucionários</h1>
                 <p>Gostou? Avalie esse projeto <Link target="_blank" href={"https://forms.gle/F8B5uyFD5piv3H3q7"} className="underline">nesse link!</Link></p>
               </div>
@@ -32,12 +30,15 @@ export default function Footer() {
                 </div>
               ))} */}
           </div>
-          <div className="mt-24 flex flex-col justify-between gap-4 border-t pt-8 text-sm font-medium text-muted-foreground md:flex-row md:items-center">
-            <p >Museu de Computação Prof.Odelar Leite Linhares
+          <div className="flex flex-col justify-between gap-4 border-t pt-8 text-sm font-medium text-muted-foreground md:flex-row md:items-center">
+            <div className="flex flex-wrap gap-4">
+              <Image src={"/logo-icmc-preto.png"} width={212} height={107} alt="ICMC" className="dark:invert" />
+              <p >Museu de Computação Prof.Odelar Leite Linhares
               <br />Instituto de Ciências e Matemáticas e de Computação - ICMC
               <br />Universidade de São Paulo - USP
               <br />Avenida Trabalhador São-Carlense, nº 400, Centro.
               <br />CEP 13566-590 - São Carlos - SP</p>
+            </div>
             <ul className="flex gap-4">
               {/* {bottomLinks.map((link, linkIdx) => (
                   <li key={linkIdx} className="underline hover:text-primary">
